b331ee76e32c4ce08c1d180634164d97618a940c
bluesaray12 <saraydefas2019@gmail.com>
2023-05-02 07:35:30 -0500

    Página realizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
+import { E404Component } from './e404/e404.component';
+import { MerchComponent } from './merch/merch.component';
+import { CollectionsComponent } from './collections/collections.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'merch', component: MerchComponent},
+  {path:'music', component: MusicComponent},
+  {path:'collections', component: CollectionsComponent},
+
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'**', component:E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
+import { E404Component } from './e404/e404.component';
+import { MerchComponent } from './merch/merch.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    MusicComponent,
+    E404Component,
+    MerchComponent,
+    CollectionsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
